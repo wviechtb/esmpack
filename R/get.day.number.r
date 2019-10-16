0dcee378a6bd6df1f5081fa35f1c1a1bf74598bd
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-10-16 17:24:02 +0200

    Separated functions from notifications.r into separate files and started on help files.

--- R/get.day.number.r
+++ R/get.day.number.r
@@ -28,19 +28,3 @@ get.day.number <- function(notification.number, beeps.per.day = 10, total.notifi
 
     return(determined.day)
 }
-
-
-
-# Construct beep numbers from notification numbers.
-get.beep.number <- function(notification.number, beeps.per.day = 10, total.notifications = 50) {
-    # Prevent notification overflow.
-    if(notification.number > total.notifications) stop("`notification.number` cannot exceed maximum number of notifications.")
-
-    # Mod.
-    mod <- notification.number %% beeps.per.day
-
-    # Determine the beep number.
-    beep.number <- ifelse(mod > 0, mod, beeps.per.day)
-
-    return(beep.number)
-}
