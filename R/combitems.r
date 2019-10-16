64f526c9c653d37442cde3492eb07dbed8b25b54
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-10-16 16:59:14 +0200

    Renamed aggitems() to combitems() and added corresponding help file.

--- R/combitems.r
+++ R/combitems.r
@@ -1,4 +1,4 @@
-aggitems <- function(items, data, min.k, na.rm=TRUE, fun=mean, verbose=TRUE) {
+combitems <- function(items, data, fun = mean, na.rm = TRUE, min.k, verbose = TRUE) {
 
    # if 'data' is not a data frame, turn it into one
 
@@ -16,14 +16,14 @@ aggitems <- function(items, data, min.k, na.rm=TRUE, fun=mean, verbose=TRUE) {
    # check that 'items' argument is either a character or a numeric vector
 
    if (!(is.character(items) | is.numeric(items)))
-      stop(mstyle$stop("Argument 'items' must either be a character or a numeric vector."))
+      stop("Argument 'items' must either be a character or a numeric vector.")
 
    if (is.character(items)) {
 
       #items.pos <- charmatch(items, varnames)
 
       items.pos <- lapply(items, function(x) {
-         pos <- grep(x, varnames, fixed=TRUE)
+         pos <- grep(x, varnames, fixed = TRUE)
          if (length(pos) == 0L) {
             return(NA)
          } else {
@@ -32,12 +32,12 @@ aggitems <- function(items, data, min.k, na.rm=TRUE, fun=mean, verbose=TRUE) {
       })
 
       if (any(is.na(items.pos)))
-         stop(ifelse(sum(is.na(items.pos)) == 1L, "Item", "Items"), " not found in the data frame: ", paste(items[is.na(items.pos)], collapse=", "), ".")
+         stop(ifelse(sum(is.na(items.pos)) == 1L, "Item", "Items"), " not found in the data frame: ", paste(items[is.na(items.pos)], collapse = ", "), ".")
 
       items.pos <- unique(unlist(items.pos))
 
       #if (any(items.pos == 0L))
-      #   stop("No ambiguous match found for ", ifelse(sum(items.pos == 0L) == 1L, "item ", "items "), "(", paste(items[items.pos == 0], collapse=", "), ").")
+      #   stop("No ambiguous match found for ", ifelse(sum(items.pos == 0L) == 1L, "item ", "items "), "(", paste(items[items.pos == 0], collapse = ", "), ").")
 
    } else {
 
@@ -59,12 +59,24 @@ aggitems <- function(items, data, min.k, na.rm=TRUE, fun=mean, verbose=TRUE) {
    # in case min.k is specified, apply the 'minimum k' rule
 
    if (!missing(min.k)) {
-      k.not.na <- apply(items, 1, FUN=function(x) sum(!is.na(x)))
+
+      if (length(min.k) != 1)
+         stop("Argument 'min.k' must be of length 1.")
+
+      if (!is.numeric(min.k))
+         stop("Argument 'min.k' must be a number.")
+
+      min.k <- round(min.k)
+
+      if (min.k < 1 || min.k > length(items.pos))
+         stop("Argument 'min.k' should be a number between 1 and the number of items to be combined.")
+
+      k.not.na <- apply(items, 1, FUN = function(x) sum(!is.na(x)))
       x[k.not.na < min.k] <- NA
    }
 
    if (verbose)
-      message(paste0("Computed '", as.character(substitute(fun)), "' for these items: ", paste0(varnames[items.pos], collapse=", "), "."))
+      message(paste0("Computed '", as.character(substitute(fun)), "' for these items: ", paste0(varnames[items.pos], collapse = ", "), "."))
 
    return(x)
 
